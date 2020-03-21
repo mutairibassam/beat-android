1af3a30c9e1fba372e0f9560cc218ae5606ac1ef
BassamMutairi <mutairibassam@gmail.com>
2020-03-21 13:17:12 +0300

    Re-orgnaizing

---
+++ S01-Order-Coffee-Calc/app/src/main/java/com/example/android/s01_coffee_order_calc/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.s01_coffee_order_calc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

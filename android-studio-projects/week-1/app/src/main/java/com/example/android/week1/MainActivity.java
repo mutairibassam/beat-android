b498e1d3279fd8c71f5f94465b6614c9b0575070
BassamMutairi <mutairibassam@gmail.com>
2020-12-26 16:37:31 +0300

    UPDATE: complete week-1 app

--- android-studio-projects/week-1/app/src/main/java/com/example/android/week1/MainActivity.java
+++ android-studio-projects/week-1/app/src/main/java/com/example/android/week1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.android.week1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,4 +17,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
 }

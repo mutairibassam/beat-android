d048547cdc5ae7bd3dc06f3c0b9d371e01d6c1ad
BassamMutairi <mutairibassam@gmail.com>
2021-01-25 18:22:03 +0300

    UPDATE: add guidance note

--- android-studio-projects/ux-app/app/src/main/java/com/datum/android/userexperienceapp/MainActivity.java
+++ android-studio-projects/ux-app/app/src/main/java/com/datum/android/userexperienceapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.datum.android.userexperienceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        AppCompatDelegate.setDefaultNightMode();
+//        getDelegate().setLocalNightMode();
     }
 }

c0c1a28ce964a9226517d8242e449545d6a5e7af
BassamMutairi <mutairibassam@gmail.com>
2021-01-28 14:59:47 +0300

    UPDATE: add intent UI testing

--- android-studio-projects/espresso-ui-testing/app/src/main/java/com/datum/android/espressouitesting/MainActivity.java
+++ android-studio-projects/espresso-ui-testing/app/src/main/java/com/datum/android/espressouitesting/MainActivity.java
@@ -6,21 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button mLogin;
+    EditText mUsername, mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mUsername = findViewById(R.id.et_username);
+        mPassword = findViewById(R.id.et_password);
+
         mLogin = findViewById(R.id.login_button);
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), MessageActivity.class));
+                Intent intent = new Intent(getApplicationContext(), MessageActivity.class);
+                intent.putExtra("username", mUsername.getText().toString());
+                intent.putExtra("password", mPassword.getText().toString());
+                startActivity(intent);
             }
         });
     }

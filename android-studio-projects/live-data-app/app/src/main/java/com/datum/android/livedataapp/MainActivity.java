23d583978931120651e9a2b82129af699adb6ad6
BassamMutairi <mutairibassam@gmail.com>
2021-03-02 21:09:45 +0300

    UPDATE: add live-data-app

---
+++ android-studio-projects/live-data-app/app/src/main/java/com/datum/android/livedataapp/MainActivity.java
@@ -0,0 +1,54 @@
+package com.datum.android.livedataapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.datum.android.livedataapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        /**
+         *  What is LiveData?
+         *  -   an observable data holder class
+         *  -   lifecycle aware which means respects the lifecycle of other app components "Activity, Fragment, Services"
+         *      to ensure LiveData update only app component observers that in active lifecycle state
+         *
+         *
+         * @Observable_data_holder_class : means that liveData can be observed by other components like UI controllers
+         *
+         * @LiveData = data is aware of the LifeCycle of it's observer (Activity/Fragment)
+         *
+         *
+         */
+
+
+        MainActivityViewModel viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+        viewModel.getRandom().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String it) {
+                binding.number.setText(it);
+            }
+        });
+
+        binding.generate.setOnClickListener(View -> {
+            viewModel.generateRandomNumber();
+        });
+
+
+    }
+}
---
+++ android-studio-projects/live-data-app/app/src/main/java/com/datum/android/livedataapp/MainActivityViewModel.java
@@ -0,0 +1,27 @@
+package com.datum.android.livedataapp;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.Random;
+
+public class MainActivityViewModel extends ViewModel {
+
+    // mutable live data should be only visible to our view model class
+    private MutableLiveData<String> myRandom;
+
+    // for public use cases we create a custom public method that return our mutable data as live data type
+    public LiveData<String> getRandom() {
+        if(myRandom == null) {
+            myRandom = new MutableLiveData<String>();
+            generateRandomNumber();
+        }
+        return myRandom;
+    }
+
+    public void generateRandomNumber() {
+        Random random = new Random();
+        myRandom.setValue("" + random.nextInt(10 - 1)+1);
+    }
+}

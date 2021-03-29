5d2d93abcec9c44915dcb46a088f924be31dfed0
BassamMutairi <mutairibassam@gmail.com>
2021-03-29 20:41:06 +0300

    UPDATE: add am-dark app

---
+++ android-studio-projects/am-dark/app/src/main/java/com/datum/android/amdark/MainActivity.java
@@ -0,0 +1,31 @@
+package com.datum.android.amdark;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.btn_mode);
+        button.setOnClickListener(View -> {
+            dayDark();
+        });
+
+    }
+
+
+    public void dayDark() {
+        if (AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_YES) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+}
